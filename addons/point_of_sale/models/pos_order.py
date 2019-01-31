f30bdf45febf630917d9b63e6388524b27054229
trananhdung <trananhdung17@gmail.com>
2019-01-31 20:29:12 +0700

    [ADD] split function create_picking for easier to override

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -781,70 +781,118 @@ class PosOrder(models.Model):
                 return False
         return True
 
+    def _prepare_picking_vals(self, order):
+        """
+
+        :param order:
+        :return:
+        """
+        StockWarehouse = self.env['stock.warehouse']
+        address = order.partner_id.address_get(['delivery']) or {}
+        picking_type = order.picking_type_id
+        return_pick_type = order.picking_type_id.return_picking_type_id or order.picking_type_id
+        location_id = order.location_id.id
+        if order.partner_id:
+            destination_id = order.partner_id.property_stock_customer.id
+        else:
+            if (not picking_type) or (not picking_type.default_location_dest_id):
+                customerloc, supplierloc = StockWarehouse._get_partner_locations()
+                destination_id = customerloc.id
+            else:
+                destination_id = picking_type.default_location_dest_id.id
+
+        if picking_type:
+            picking_vals = False
+            return_vals = False
+
+            vals = {
+                'origin': order.name,
+                'partner_id': address.get('delivery', False),
+                'date_done': order.date_order,
+                'picking_type_id': picking_type.id,
+                'company_id': order.company_id.id,
+                'move_type': 'direct',
+                'note': order.note or "",
+                'location_id': location_id,
+                'location_dest_id': destination_id,
+            }
+            pos_qty = any([x.qty > 0 for x in order.lines if x.product_id.type in ['product', 'consu']])
+            if pos_qty:
+                picking_vals = vals.copy()
+                # order_picking = Picking.create(picking_vals.copy())
+                # order_picking.message_post(body=message)
+            neg_qty = any([x.qty < 0 for x in order.lines if x.product_id.type in ['product', 'consu']])
+            if neg_qty:
+                return_vals = vals.copy()
+                return_vals.update({
+                    'location_id': destination_id,
+                    'location_dest_id': return_pick_type != picking_type and return_pick_type.default_location_dest_id.id or location_id,
+                    'picking_type_id': return_pick_type.id
+                })
+            return picking_vals, return_vals
+        else:
+            return False, False
+
+    def _prepare_move_vals(self, order, line, order_picking, return_picking):
+        """
+
+        :param order:
+        :param line:
+        :param picking:
+        :return:
+        """
+        StockWarehouse = self.env['stock.warehouse']
+        picking_type = order.picking_type_id
+        return_pick_type = order.picking_type_id.return_picking_type_id or order.picking_type_id
+        location_id = order.location_id.id
+        if order.partner_id:
+            destination_id = order.partner_id.property_stock_customer.id
+        else:
+            if (not picking_type) or (not picking_type.default_location_dest_id):
+                customerloc, supplierloc = StockWarehouse._get_partner_locations()
+                destination_id = customerloc.id
+            else:
+                destination_id = picking_type.default_location_dest_id.id
+
+        vals = {
+            'name': line.name,
+            'product_uom': line.product_id.uom_id.id,
+            'picking_id': order_picking.id if line.qty >= 0 else return_picking.id,
+            'picking_type_id': picking_type.id if line.qty >= 0 else return_pick_type.id,
+            'product_id': line.product_id.id,
+            'product_uom_qty': abs(line.qty),
+            'state': 'draft',
+            'location_id': location_id if line.qty >= 0 else destination_id,
+            'location_dest_id': destination_id if line.qty >= 0 else \
+                return_pick_type != picking_type and return_pick_type.default_location_dest_id.id or location_id,
+        }
+        return vals
+
     def create_picking(self):
         """Create a picking for each order and validate it."""
         Picking = self.env['stock.picking']
         Move = self.env['stock.move']
-        StockWarehouse = self.env['stock.warehouse']
+        # StockWarehouse = self.env['stock.warehouse']
         for order in self:
             if not order.lines.filtered(lambda l: l.product_id.type in ['product', 'consu']):
                 continue
-            address = order.partner_id.address_get(['delivery']) or {}
-            picking_type = order.picking_type_id
-            return_pick_type = order.picking_type_id.return_picking_type_id or order.picking_type_id
+            message = _(
+                "This transfer has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (
+                          order.id, order.name)
             order_picking = Picking
             return_picking = Picking
             moves = Move
-            location_id = order.location_id.id
-            if order.partner_id:
-                destination_id = order.partner_id.property_stock_customer.id
-            else:
-                if (not picking_type) or (not picking_type.default_location_dest_id):
-                    customerloc, supplierloc = StockWarehouse._get_partner_locations()
-                    destination_id = customerloc.id
-                else:
-                    destination_id = picking_type.default_location_dest_id.id
 
-            if picking_type:
-                message = _("This transfer has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (order.id, order.name)
-                picking_vals = {
-                    'origin': order.name,
-                    'partner_id': address.get('delivery', False),
-                    'date_done': order.date_order,
-                    'picking_type_id': picking_type.id,
-                    'company_id': order.company_id.id,
-                    'move_type': 'direct',
-                    'note': order.note or "",
-                    'location_id': location_id,
-                    'location_dest_id': destination_id,
-                }
-                pos_qty = any([x.qty > 0 for x in order.lines if x.product_id.type in ['product', 'consu']])
-                if pos_qty:
-                    order_picking = Picking.create(picking_vals.copy())
-                    order_picking.message_post(body=message)
-                neg_qty = any([x.qty < 0 for x in order.lines if x.product_id.type in ['product', 'consu']])
-                if neg_qty:
-                    return_vals = picking_vals.copy()
-                    return_vals.update({
-                        'location_id': destination_id,
-                        'location_dest_id': return_pick_type != picking_type and return_pick_type.default_location_dest_id.id or location_id,
-                        'picking_type_id': return_pick_type.id
-                    })
-                    return_picking = Picking.create(return_vals)
-                    return_picking.message_post(body=message)
+            picking_vals, return_vals = self._prepare_picking_vals(order)
+            if picking_vals:
+                order_picking = Picking.create(picking_vals)
+                order_picking.message_post(body=message)
+            if return_vals:
+                return_picking = Picking.create(return_vals)
+                return_picking.message_post(body=message)
 
             for line in order.lines.filtered(lambda l: l.product_id.type in ['product', 'consu'] and not float_is_zero(l.qty, precision_rounding=l.product_id.uom_id.rounding)):
-                moves |= Move.create({
-                    'name': line.name,
-                    'product_uom': line.product_id.uom_id.id,
-                    'picking_id': order_picking.id if line.qty >= 0 else return_picking.id,
-                    'picking_type_id': picking_type.id if line.qty >= 0 else return_pick_type.id,
-                    'product_id': line.product_id.id,
-                    'product_uom_qty': abs(line.qty),
-                    'state': 'draft',
-                    'location_id': location_id if line.qty >= 0 else destination_id,
-                    'location_dest_id': destination_id if line.qty >= 0 else return_pick_type != picking_type and return_pick_type.default_location_dest_id.id or location_id,
-                })
+                moves |= Move.create(self._prepare_move_vals(order, line, order_picking, return_picking))
 
             # prefer associating the regular order picking, not the return
             order.write({'picking_id': order_picking.id or return_picking.id})
